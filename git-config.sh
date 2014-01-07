6c0f9f53c233b9b321b7d3a525d1ce2efb2f64a9
Guilherme Rodrigues <gadr90@gmail.com>
2014-01-07 14:51:22 -0200

    Update push default to current

--- git-config.sh
+++ git-config.sh
@@ -4,5 +4,6 @@ git config --global user.email "gadr90@gmail.com"
 git config --global alias.changelog 'log --oneline --abbrev-commit --no-merges'
 git config --global alias.graph 'log --oneline --graph --decorate'
 git config --global color.ui true
+git config --global push.default current
 
 echo "Applied git configurations."
