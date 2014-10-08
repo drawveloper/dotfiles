b91b1b18d353339fd79e6e1459a690598d05632c
Guilherme Rodrigues <gadr90@gmail.com>
2014-10-08 10:25:38 -0300

    add tree and autojump

--- setup-initial.sh
+++ setup-initial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 apt-get update
-apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar
+apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump
 
 echo ">>>" 
 echo ">>> Finished installing basics." 
