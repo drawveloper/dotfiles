da7c338711a89b3ebbc83898e72c7fb3d3196f16
Guilherme Rodrigues <gadr90@gmail.com>
2015-04-03 13:53:15 -0300

    Replace tilde for HOME env var

--- setup-node.sh
+++ setup-node.sh
@@ -2,12 +2,12 @@
 echo 'Installing node latest stable'
 
 echo 'export PATH=$HOME/local/bin:$PATH' >> ~/.bashrc
-. ~/.bashrc
-mkdir ~/local
-mkdir ~/node-latest-install
-cd ~/node-latest-install
+. $HOME/.bashrc
+mkdir $HOME/local
+mkdir $HOME/node-latest-install
+cd $HOME/node-latest-install
 curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
-./configure --prefix=~/local
+./configure --prefix=$HOME/local
 make install
 
 # Source our bash in order to have node at our path.
