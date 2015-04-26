cab8923ab3fec2dd21cecbc5bf858ac68fd1ea6a
Guilherme Rodrigues <gadr90@gmail.com>
2015-04-26 20:48:06 -0300

    Add python utils to init script

--- setup-initial.sh
+++ setup-initial.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 apt-get update
-apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump
+apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump python-pip python-dev
 
-echo ">>>" 
-echo ">>> Finished installing basics." 
+echo ">>>"
+echo ">>> Finished installing basics."
 echo ">>>"
 
-if uname -a | grep VirtualBox 
+if uname -a | grep VirtualBox
 then
     echo ">>>"
     echo ">>> Detected VirtualBox - installing guest additions"
     echo ">>>"
-    
+
     sudo apt-get install linux-headers-$(uname -r)
     sudo apt-get install virtualbox-guest-x11
-    
+
     echo ">>>"
     echo ">>> Finished installing guest additions"
     echo ">>>"
