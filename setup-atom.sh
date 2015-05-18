4d1b9ba7837668bcadc92a9e91cf9b2ada6d91b6
Guilherme Rodrigues <gadr90@gmail.com>
2015-05-18 14:13:22 -0300

    Update setup-atom.sh

--- setup-atom.sh
+++ setup-atom.sh
@@ -16,8 +16,8 @@ script/build
 sudo script/grunt install
 
 # Add packages
-apm install autocomplete-plus linter color-picker atom-beautify \
+apm install linter color-picker atom-beautify \
 file-icons emmet merge-conflicts zen editorconfig \
 sublime-style-column-selection regex-railroad-diagram git-log \
-todo-show git-tab-status tabs-to-spaces angularjs save-session \
-git-history go-plus react travis-ci-status 
+todo-show git-tab-status tabs-to-spaces \
+git-history react 
