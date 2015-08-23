28a268d566e22539c372c354f4318d3b455cc5e0
Guilherme Rodrigues <gadr90@gmail.com>
2015-08-23 12:48:30 +0200

    Use apm stars :star:

--- setup-apm.sh
+++ setup-apm.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-echo ">>> Installing atom packages"
+echo ">>> Log into apm and install starred packages"
 
-apm install \
-linter \
-linter-eslint \
-linter-flake8 \
-color-picker \
-atom-beautify \
-file-icons \
-emmet \
-merge-conflicts \
-zen \
-editorconfig \
-sublime-style-column-selection \
-regex-railroad-diagram \
-git-log \
-todo-show \
-git-tab-status \
-tabs-to-spaces \
-git-history \
-react
+apm login
+apm stars --install
 
 echo ">>>"
 echo ">>> Installed atom packages."
