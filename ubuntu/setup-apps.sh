407ea51f13e6c422f179563c9738a21ab6bee6ec
Guilherme Rodrigues <gadr90@gmail.com>
2016-03-25 19:24:20 +0100

    Add terminator

--- ubuntu/setup-apps.sh
+++ ubuntu/setup-apps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 apt-get update
-apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump python-pip python-dev shellcheck fish
+apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump python-pip python-dev shellcheck fish terminator
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 apt-get -y nodejs
 
