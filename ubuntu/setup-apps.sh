3b7e599fbe27459e77a564bca4653fd9c5fd7201
Guilherme Rodrigues <gadr90@gmail.com>
2016-03-25 18:33:37 +0100

    Update setup-apps.sh

--- ubuntu/setup-apps.sh
+++ ubuntu/setup-apps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 apt-get update
-apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump python-pip python-dev shellcheck fish nodejs
+apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump python-pip python-dev shellcheck fish
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+apt-get -y nodejs
 
 echo ">>>"
 echo ">>> Finished installing basics."
