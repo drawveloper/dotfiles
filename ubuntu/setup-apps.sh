25ae68dd847de87a45e984f94196414d7dc47e82
Guilherme Rodrigues <gadr90@gmail.com>
2016-09-10 12:59:00 -0300

    Update setup-apps.sh

--- ubuntu/setup-apps.sh
+++ ubuntu/setup-apps.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+
+echo ">>>"
+echo ">>> Installing apps."
+echo ">>>"
+
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
 apt-get update
-apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump python-pip python-dev shellcheck fish terminator
-curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-apt-get -y nodejs
+apt-get -y --ignore-missing install git-core pkg-config build-essential curl gcc g++ checkinstall openssh-server vim xclip p7zip-full p7zip-rar tree autojump shellcheck fish terminator
 
 echo ">>>"
 echo ">>> Finished installing basics."
+echo ">>>"
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get -y install nodejs
+
+echo ">>>"
+echo ">>> Finished installing node."
+echo ">>>"
+
+add-apt-repository ppa:webupd8team/atom
+apt-get update
+apt-get install atom
+
+echo ">>>"
+echo ">>> Finished installing atom."
 echo ">>>"
